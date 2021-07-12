65be847c8cd4b8e4b0f8f734c111b1871e356f93
vantoan <lvtoan.it2000@gmail.com>
2021-07-12 19:30:18 +0700

    simple-web

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ var morgan = require('morgan');
 var exphbs  = require('express-handlebars');
 // Nạp thư viện path
 var path = require('path')
+
+var sass = require('sass')
 // Gọi func như dưới, tương ứng sẽ tạo ra một instance gán cho app
 // Như một đối tượng, sự dụng xuyên suốt trong quá trình xây dựng và chạy chương trình
 const app = express()
 // Cổng 
 const port = 3000
 
+// Xử lý static file
+app.use(express.static(path.join(__dirname,"public")));
 
 // HTTP logger
 app.use(morgan('combined'))
@@ -19,10 +23,13 @@ app.use(morgan('combined'))
 // Template engine
 // Định nghĩa handlebars là exphbs()
 app.engine('handlebars', exphbs());
+
 //Set view engine giống handlebars
 app.set('view engine', 'handlebars');
+
 // Reset đường dẫn
 app.set('views', path.join(__dirname, 'resources/views/'));
+
 // Đường dẫn - hướng (route)
 // func thường có dạng func(){...}
 // Dưới đây ở dạng arrow func
